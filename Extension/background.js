47efdb7dc9bd453ff7b77ce6896f10c99cdf1a13
Helen <helenli2727@gmail.com>
2018-09-21 21:51:07 -0400

    added example code in background.js

--- Extension/background.js
+++ Extension/background.js
@@ -1,10 +1,10 @@
- {
-    "name": "Getting Started Example",
-    "version": "1.0",
-    "description": "Build an Extension!",
-    "background": {
-      "scripts": ["background.js"],
-      "persistent": false
-    },
-    "manifest_version": 2
-  }
+// example code
+
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
+    console.log("The color is green.");
+  });
+});
+
